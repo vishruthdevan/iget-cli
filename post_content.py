9a9a7b0ec114b4147d882cb7276c0c5a21afcacb
Vishruth <vishruthdevan@gmail.com>
2022-02-07 17:56:10 +0530

    feat: add function to publish media content

--- post_content.py
+++ post_content.py
@@ -27,3 +27,25 @@ def get_media_object_status(media_id, params):
     endpoint_params['access_token'] = params['access_token']
 
     return make_api_call(url, endpoint_params, 'GET')
+
+
+def publish_media(media_id, params):
+    url = params['endpoint_base'] + params['instagram_account_id'] + \
+        '/media_publish'
+
+    endpoint_params = dict()
+    endpoint_params['creation_id'] = media_id
+    endpoint_params['access_token'] = params['access_token']
+
+    return make_api_call(url, endpoint_params, 'POST')
+
+# params = get_creds()
+# params['media_type'] = "IMAGE"
+# params['caption'] = "test"
+# params['media_url'] = 'https://workmacro.com/wp-content/uploads/2018/02/4-by-5-819x1024.png'
+
+# pp = pprint.PrettyPrinter(indent=2)
+
+# # media_id = create_media_object(params)
+# # print(media_id)
+# publish_media('17919828491084241', params)
